4ba779cf0846c8dbe676dbc489fdb241354c7e38
Jesse Mazzie <jessegalt@yahoo.com>
2018-04-06 00:04:46 -0400

    Refactored code into packages for clarity.

--- Server.java
+++
@@ -1,6 +0,0 @@
-import java.util.Hashtable;
-
-public class Server {
-    private Hashtable<String, User> users; //TODO: Make this typed <String, User>
-
-}
--- User.java
+++
@@ -1,8 +0,0 @@
-import java.util.Vector;
-
-public class User {
-    String username;
-    String password;
-    Vector<String> friendsList;
-    CTC serverConnection;
-}
