6002cf2cd50d0b68f3c9e48fed976d9bbe7b07ff
Jesse Mazzie <jessegalt@yahoo.com>
2018-04-05 23:51:10 -0400

    Added User class.

--- Server.java
+++ Server.java
@@ -1,2 +1,6 @@
+import java.util.Hashtable;
+
 public class Server {
+    private Hashtable<String, User> users; //TODO: Make this typed <String, User>
+
 }
---
+++ User.java
@@ -0,0 +1,8 @@
+import java.util.Vector;
+
+public class User {
+    String username;
+    String password;
+    Vector<String> friendsList;
+    CTC serverConnection;
+}
